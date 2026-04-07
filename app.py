8a4825e823a008cc375d51f4dc9c4005a8114ad1
Wilder407 <jahn.lisa@gmail.com>
2026-04-07 13:11:46 +0000

    updated syntax

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World from Lisa Wilder in 3308!'
 
-@app_route('/db_test')
+@app.route('/db_test')
 def testing():
     conn = pyscopg2.connect("postgresql://basketball_1xwx_user:P0P32rPoGNUdTMSUjkyaEZXWEpVSYd89@dpg-d7aflcvfte5s73b45d20-a/basketball_1xwx")
     conn.close()
