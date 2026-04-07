f08d38ec66dcee6e24218b028f06078165697948
Wilder407 <jahn.lisa@gmail.com>
2026-04-07 12:41:44 +0000

    Testing def created, first push to render

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import pyscopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World from Lisa Wilder in 3308!'
+
+@app_route('/db_test')
+def testing():
+    conn = pyscopg2.connect(postgresql://basketball_1xwx_user:P0P32rPoGNUdTMSUjkyaEZXWEpVSYd89@dpg-d7aflcvfte5s73b45d20-a/basketball_1xwx)
+    conn.close()
+    return "Database connection successful."
