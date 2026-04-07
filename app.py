67b8b899ba9a509128fba08761ae6d90d80e32c6
Wilder407 <jahn.lisa@gmail.com>
2026-04-07 13:17:24 +0000

    updated syntax

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ def hello_world():
 
 @app.route('/db_test')
 def testing():
-    conn = pyscopg2.connect("postgresql://basketball_1xwx_user:P0P32rPoGNUdTMSUjkyaEZXWEpVSYd89@dpg-d7aflcvfte5s73b45d20-a/basketball_1xwx")
+    conn = psycopg2.connect("postgresql://basketball_1xwx_user:P0P32rPoGNUdTMSUjkyaEZXWEpVSYd89@dpg-d7aflcvfte5s73b45d20-a/basketball_1xwx")
     conn.close()
     return "Database connection successful."
